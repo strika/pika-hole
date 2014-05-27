f8836768608a75469cb666b53d5adfe9b2adee05
Tanja Stjepanović Stričević <stricevictanja@gmail.com>
2014-05-27 23:41:53 +0200

    Slide in video

--- public/design/js/application.js
+++ public/design/js/application.js
@@ -2,6 +2,19 @@ $(function() {
 	var toggleCheckbox = function() {
 		$(this).toggleClass("glyphicon-unchecked glyphicon-check");
 	};
+
+	var showVideo = function() {
+		var options = { direction: "right" },
+			$video = $(".video-full"),
+			$body = $("body");
+		
+		$body.css({ overflow: "hidden" });
+		$video.css({ width: $(window).width(), height: $(window).height() });
+		$video.toggle("slide", options, 200 );
+		
+		return false;
+	};
 	
 	$("span.glyphicon").click(toggleCheckbox);
+	$("a").click(showVideo);
 });
